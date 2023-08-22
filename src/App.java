7b26c4869fb15c8b8149ccf173a97cbdd5539e3c
gpdmadhushani <madhushani577@gmail.com>
2023-08-22 18:52:01 +0530

    Student Crud

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sound.midi.SysexMessage;
+
 import org.hibernate.Session;
 
 import entity.StudentEntity;
@@ -10,18 +12,31 @@ import util.SessionFactoryConfiguration;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       StudentRepository repo=new StudentRepository();
-       StudentEntity entity=new StudentEntity();
-       entity.setNic("869541326V");
-       StudentName studentName=new StudentName("stu 1","stu last 1");
-
+      // student save
+        StudentRepository repo=new StudentRepository();
+      /* StudentEntity entity=new StudentEntity();
+       entity.setNic("769441326V");
+       StudentName studentName=new StudentName("stu 3","stu last 3");
        entity.setName(studentName);
        List <String> mobiles=new ArrayList<>();
-       mobiles.add("0778569412");
-       mobiles.add("0715213647");
+       mobiles.add("0788564102");
+       mobiles.add("0715240247");
        entity.setMobiles(mobiles);
+       repo.saveStudent(entity);*/
 
-       repo.saveStudent(entity);
+       //Student get
+
+      StudentEntity studentEntity=repo.getStudent(3);
+      System.out.println(studentEntity.toString());
+      studentEntity.setNic("7809541326V");
+      studentEntity.getName().setFirstName("updated first name");
+
+      repo.updateStudent(studentEntity);
+      studentEntity=repo.getStudent(3);
+      System.out.println(studentEntity.toString());
+      repo.deleteStudent(studentEntity);
+
+
 
 
     }
